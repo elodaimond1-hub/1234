85dabb71af3afea12844ca769a8457a2fa0005f7
elodaimond1-hub <elodaimond1@gmail.com>
2025-10-13 03:11:15 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/123/',
+  base: '/1234/',
   build: {
     chunkSizeWarningLimit: 10000,
   }
